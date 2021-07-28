d632b74b3a480891b1321775993f22203e43b2ce
seacrest <cassiep1986@gmail.com>
2021-07-28 10:29:56 -0700

    added load image, adjusted paths

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,14 +7,8 @@ const FILES_TO_CACHE = [
     "/assets/js/db.js",
     "/assets/js/index.js",
     "/assets/js/loadImages.js",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
-    "/assets/images/icons/icon-128x128.png",
-    "/assets/images/icons/icon-144x144.png",
     "/assets/images/icons/icon-152x152.png",
     "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
   ];
   
